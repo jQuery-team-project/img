97113c8c62cabf24e158d62a85a88cb83aacfa61
Minseong0000 <dave711@naver.com>
2024-05-17 16:02:36 +0900

    update

--- mainpage.js
+++ mainpage.js
@@ -128,5 +128,17 @@ ScrollTrigger.create({
   markers: false
 });
 
+// ========================mobile
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
 
 
